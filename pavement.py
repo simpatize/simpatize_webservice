6dea9091812c56aa49ebbda4eaa854102adfb3f5
mvfontes <mfontes@thoughtworks.com>
2015-12-30 14:48:48 -0300

    added task 'test'and ''migrate' to pavement.py

--- pavement.py
+++ pavement.py
@@ -1,6 +1,18 @@
-from paver.easy import task
+from paver.easy import task, needs
 from paver.shell import sh
 
 @task
 def runserver():
     sh('python manage.py runserver 0.0.0.0:8000')
+
+
+@task
+def migratedb():
+    sh('python manage.py migrate')
+
+
+@task
+@needs('migratedb')
+def test():
+    sh('python manage.py test')
+
