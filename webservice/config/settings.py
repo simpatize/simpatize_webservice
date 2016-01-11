eca96b8ecd6a153e1eb6cf281279abd1fc88b9b5
Abraao Alcantara <sephioh@gmail.com>
2016-01-11 18:01:15 -0300

    renamed config directory

--- webservice/config/settings.py
+++ webservice/config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'webservice.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'webservice.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
