3884d10dbe95dcc46eb4e4262b83dffade663cd6
Abraao Alcantara <sephioh@gmail.com>
2016-01-11 17:59:11 -0300

    created webservice django project

--- pavement.py
+++ pavement.py
@@ -3,16 +3,16 @@ from paver.shell import sh
 
 @task
 def runserver():
-    sh('python manage.py runserver 0.0.0.0:8000')
+    sh('python manage.py runserver 0.0.0.0:8000', cwd='webservice/')
 
 
 @task
 def migratedb():
-    sh('python manage.py migrate')
+    sh('python manage.py migrate', cwd='webservice/')
 
 
 @task
 @needs('migratedb')
 def test():
-    sh('python manage.py test')
+    sh('python manage.py test', cwd='webservice/')
 
