a4801eae7cf570662bd05ca255acad386e35708b
Felipe Martins <fmmartin@thoughtworks.com>
2016-03-08 13:43:43 -0300

    #51 [Vanessa, Felipe] Modified endpoint /places/ to filter places by typeatom

--- webservice/places/tests.py
+++ webservice/places/tests.py
@@ -8,9 +8,13 @@ django.setup()
 
 class PlaceApiTest(APITestCase):
     def test_get_places_endpoint_should_return_all_places(self):
+        PlaceFactory(place_type="bar")
+        PlaceFactory(place_type="bar")
+        PlaceFactory(place_type="restaurant")
         response = self.client.get('/places/')
 
-        self.assertEqual(200, response.status_code)
+        self.assertEqual(HTTP_200_OK, response.status_code)
+        self.assertEqual(3, len(response.data), 'test')
 
     def test_filter_place_by_type_should_return_many_places_of_that_type(self):
         PlaceFactory(place_type="bar")
